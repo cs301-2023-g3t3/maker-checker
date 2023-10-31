688936cbb4a5732dd7a28e0dd959dfb451c2aede
Yao Long <yaolong.tan.2021@smu.edu.sg>
2023-11-01 01:42:43 +0800

    chore: undo apigateway auth

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,7 +70,7 @@ resource "aws_api_gateway_method" "this" {
   rest_api_id      = data.aws_ssm_parameter.rest_api_id.value
   resource_id      = aws_api_gateway_resource.this.id
   http_method      = "ANY"
-  authorization    = "NONE"
-  # authorizer_id    = "kjkxid"
+  authorization    = "CUSTOM"
+  authorizer_id    = "kjkxid"
   api_key_required = false
 }
