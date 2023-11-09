8d38efb502d5b54fd946651acf21e20a312ea56c
jeremy chow <xelloxzxd@gmail.com>
2023-11-09 22:51:24 +0800

    add logging

--- go.mod
+++ go.mod
@@ -62,6 +62,7 @@ require (
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
---
+++ middleware/loggerMiddleware.go
@@ -0,0 +1,94 @@
+package middleware
+
+import (
+	// "encoding/json"
+	// "fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"makerchecker-api/models"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+type CustomData struct {
+	Method  string
+	URL     string
+	Headers map[string][]string
+}
+
+func LoggingMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		// Starting time request
+		startTime := time.Now()
+
+		// Process the request
+		ctx.Next()
+
+		// End Time request
+		endTime := time.Now()
+
+		// Execution time
+		latencyTime := endTime.Sub(startTime).Milliseconds()
+
+		// Request data
+		reqMethod := ctx.Request.Method
+		reqUri := ctx.Request.RequestURI
+		statusCode := ctx.Writer.Status()
+		metadata, ok := ctx.Request.Context().Value("RequestMetadata").(models.RequestMetadata)
+		var userAgent string
+		var sourceIP string
+		if ok {
+			// Access the UserAgent and SourceIP
+			userAgent = metadata.UserAgent
+			sourceIP = metadata.SourceIP
+		}
+
+		// Logs for makerchecker entries
+		if strings.Contains(reqUri, "/record") {
+			var action string
+			data, _ := ctx.Get("makerchecker")
+			makerchecker, _ := data.(models.Makerchecker)
+			makecheckerFields := log.Fields{
+				"id":           makerchecker.Id,
+				"makerId":      makerchecker.MakerId,
+				"makerEmail":   makerchecker.MakerEmail,
+				"checkerId":    makerchecker.CheckerId,
+				"checkerEmail": makerchecker.CheckerEmail,
+				"endpoint":     makerchecker.Endpoint,
+				"status":       makerchecker.Status,
+				"data":         makerchecker.Data,
+			}
+
+			if reqMethod == http.MethodPost {
+				action = "create"
+				log.WithFields(log.Fields{
+					"METHOD":               reqMethod,
+					"URI":                  reqUri,
+					"STATUS":               statusCode,
+					"LATENCY":              latencyTime,
+					"MAKERCHECKER_DETAILS": makecheckerFields,
+					"ACTION":               action,
+					"USER_AGENT":           userAgent,
+					"SOURCE_IP":            sourceIP,
+				}).Info("CREATE MAKERCHECKER REQUEST")
+			}
+
+			if reqMethod == http.MethodPut {
+				log.WithFields(log.Fields{
+					"METHOD":               reqMethod,
+					"URI":                  reqUri,
+					"STATUS":               statusCode,
+					"LATENCY":              latencyTime,
+					"MAKERCHECKER_DETAILS": makecheckerFields,
+					"ACTION":               makecheckerFields["status"],
+					"USER_AGENT":           userAgent,
+					"SOURCE_IP":            sourceIP,
+				}).Info("UPDATE MAKERCHECKER REQUEST")
+			}
+
+		}
+	}
+}
