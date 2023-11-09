4970ccb055904449f08fe94a9c4ade9d2fbcc531
jeremy chow <xelloxzxd@gmail.com>
2023-11-09 23:06:11 +0800

    add json logging

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"makerchecker-api/configs"
 	"makerchecker-api/routes"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func init() {
 	configs.ConnectToRedis()
 	configs.EnvMongoUri()
+	log.SetFormatter(&log.JSONFormatter{})
 }
 
 func main() {
